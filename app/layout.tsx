6abe57aaa14f90aa1ad38f742b67b863ce015231
Muhammad Shakir Khan <shakir.sedoc@gmail.com>
2026-06-23 13:52:13 +0500

    Feat: Added google analytics code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Script from 'next/script';
 import './globals.css';
 import { Navbar } from '@/components/Navbar';
 import { Footer } from '@/sections/Footer';
@@ -36,6 +37,18 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.className}>
       <body>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-BD62VFL3WE"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-BD62VFL3WE');
+          `}
+        </Script>
         <ClientProviders>
           <Navbar />
           {children}
