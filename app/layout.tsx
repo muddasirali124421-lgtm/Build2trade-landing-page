c4f341398bbdca2f89396ccfa0b82b6826fe2f48
Muhammad Shakir Khan <shakir.sedoc@gmail.com>
2026-07-13 17:23:25 +0500

    Feat: Facebook script

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,9 @@ export const metadata: Metadata = {
   icons: {
     icon: '/favicon.ico',
   },
+  other: {
+    'facebook-domain-verification': 'twhch855g4oq7cm9ixqw7pumfnfryz',
+  },
 };
 
 export default function RootLayout({
@@ -49,6 +52,26 @@ export default function RootLayout({
             gtag('config', 'G-BD62VFL3WE');
           `}
         </Script>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1709394657091733');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img height="1" width="1" style={{display:'none'}}
+            src="https://www.facebook.com/tr?id=1709394657091733&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <ClientProviders>
           <Navbar />
           {children}
